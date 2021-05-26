09a89622b5387109625a77bcacad20e5aa3839c5
A-Mosher <adam.l.mosher@gmail.com>
2021-05-25 21:48:17 -0400

    Added routes folder with auth.js file, models folder with profile.js and user.js, inside models we added a middleware folder with admin.js file and auth.js file

---
+++ models/user.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const Joi = require('Joi');
+const { User } = require('../models/user');
+const bcrypt = require('bcrypt');
+const auth = require('..middleware/auth');
+const express = require('express');
+const router = express.Router();
+
+
+//new user post endpoint
+router.post('/', async (req, res) => {
+    try {
+        const { error } = validateUser(req.body);
+
+        if (error) return res.status(400).send(error.details[0].message);
+
+        let user = await User.findOne({ email: req.body.email });
+        if (user) return res.status(400).send('User already registered.');
+
+        const salt = await bcrypt.genSalt(10);
+        user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: await bcrypt.hash(req.body.password, salt),
+        });
+
+        await user.save();
+
+        const token = user.generateAuthToken();
+
+        return res
+            .header('x-auth-token', token)
+            .header('access-control-expose-headers', 'x-auth-token')
+            .send({ _id: user._id, name: user.name, email: user.email });
+    }   catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true, minlength: 5, maxlength:50 },
+    email: { type: String, unique: true, required: true, minlength: 5, maxlength: 255 },
+    password: { type: String, required: true, maxlength: 1024, minlength: 5 },
+    isAdmin: { type: Boolean, default: false },
+});
+
+userSchema.methods.generateAuthToken = function () {
+    return jwt.sign({ _id: this._id, name: this.name, isAdmin: this.isAdmin }, config.get('jwtSecret'));
+};
+
+const User = mongoose.model('User', userSchema);
+
+function validateUser(user) {
+    const schema = Joi.object({
+        name: Joi.string().min(5).max(50).required(),
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(1024).required(),
+    });
+    return schema.validate(user);
+}
+
+module.exports = router;
+exports.User = User;
+exports.validate = validateUser;
